c2d730dc208f3ac8c24df6b6a97d59a2a2124ca0
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-08 15:36:44 -0700

    My 7th C-P commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long my_long_int;
+	long long my_int;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("size of a int: %zu byte(s)\n", sizeof(i));
+	printf("size of a long int: %zu byte(s)\n", sizeof(my_long_int));
+	printf("size of long long int: %zu byte(s)\n", sizeof(my_int));
+	return (0);
+}
