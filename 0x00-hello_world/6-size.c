e8996de69659d3a871663a54ae36df9881327270
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-08 16:02:40 -0700

    My C-P commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("size of an int: %d byte(s)\n", sizeof(i));
-	printf("size of a long int: %d byte(s)\n", sizeof(my_long_int));
-	printf("size of a long long int: %d byte(s)\n", sizeof(my_int));
-	printf("size of a float: %d byte(s)\n", sizeof(f));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s)\n", sizeof(my_long_int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(my_int));
+	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
