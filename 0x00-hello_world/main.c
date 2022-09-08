5f7c1ecc4ee673176fc96473ff7c84b7c0ae9fe9
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-08 09:04:03 -0700

    My 2nd C-P commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
-  *main - Emtry point
-  *
-  *Return: Always 0 (Success)
-  */
-int main(void)
+ * main - Entry Point
+ *
+ * Return: Always 0 (succes)
+ */
+int main (void)
 {
-	        return (0);
+	return (0);
 }
