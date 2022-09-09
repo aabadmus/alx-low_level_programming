6de9a95e289fac2e0fb7e465a376e58714c00e84
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-09 14:25:37 -0700

    My 1st commit on if else and while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-	printf("n is positive \n");
+	printf("%d is positive \n");
 	}
 	if (n == 0)
 	{
-	printf("n is zero \n");
+	printf("%d is zero \n");
 	}
 	if (n < 0)
 	{
-	printf("n is negative \n");
+	printf("%d positived is negative \n");
 	}
 	return (0);
 }
