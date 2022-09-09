99ff21337c07a5417e4050e0abc872da405391d6
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-09 15:57:49 -0700

    My 2nd if,else and where commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,21 +8,22 @@
  */
 int main(void)
 {
-	int n, digit;
+	int n;
+	int lastDigit = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	if (n == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, digit);
+	printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	if (n < 6 && n != 0)
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 	return (0);
 }
