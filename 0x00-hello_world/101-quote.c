4e5897f5e4a5256f382b6473aab4e9850f654773
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-09 01:29:46 -0700

    My 9th C-P commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Alway 1 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	char kunle[59]
-
-	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
