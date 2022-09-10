7b1961bb98ec44dd67db515fb450396d08d45296
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-09 18:21:52 -0700

    My 6th commit on if, else and while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	printf("%u/n", x);
+	return (0);
+}
