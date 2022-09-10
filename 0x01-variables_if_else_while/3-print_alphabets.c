f1b7c1287e41d172da35ab4f8fc6f03ebe65f205
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-09 17:37:24 -0700

    My 4th commit on if, else and while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+	x = tolower(x);
+	putchar(x);
+	x++;
+	}
+	while (x <= 'Z')
+	{
+	x = toupper(x);
+	putchar(x);
+	x++;
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
