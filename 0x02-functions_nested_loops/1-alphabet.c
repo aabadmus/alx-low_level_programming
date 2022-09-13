08b0618fe2f8df34db77ca8e8e39d011af9f1d2c
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-13 08:45:41 -0700

    My 2nd commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+
+{
+
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+
+}
