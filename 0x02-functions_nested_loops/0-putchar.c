a97e23be0b9986296e1297684021d6296b695cc5
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-13 06:04:12 -0700

    My first commit on Nested looping

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+
+int main(void)
+
+{
+char item[] = "_putchar";
+
+int i = 0;
+
+while (item[i] != '\0')
+{
+	_putchar(item[i]);
+	i++;
+}
+	_putchar('\n');
+
+	return (0);
+}
