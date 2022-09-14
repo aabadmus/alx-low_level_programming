ab9514d41d89148bbd2a0696fbf52c27ebbfc875
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-14 14:16:12 -0700

    My 16th commit

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,26 +8,23 @@
  * Return: Always 0 (Success)
  */
 
-
-
 int main(void)
 {
+	long int n1, n2, fn, afn;
 
-	long int x = 1;
-	long int y = 2;
-	long int nth = x + y;
-	long sum = 0;
-
-	while (nth < 4000000)
+	n1 = 1;
+	n2 = 2;
+	fn = afn = 0;
+	while (fn <= 4000000)
 	{
-		if (nth % 2 == 0)
+		fn = n1 + n2;
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
 		{
-			sum = nth + sum;
+			afn += n1;
 		}
-		x = y;
-		y = nth;
-		nth = x + y;
 	}
-	printf("%lu\n", sum);
+	printf("%ld\n", afn);
 	return (0);
 }
