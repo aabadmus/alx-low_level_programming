bef68e4ec1f3f9538f7120b0ff011d3c96226769
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-13 17:39:38 -0700

    My 7th commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit
+ * @n: is the letter being tested
+ *
+ * Return: Always 4`
+ */
+
+int print_last_digit(int n)
+{
+	int r;
+
+	if (n >= 0)
+	{
+		r = ((n % 10) + 48);
+		_putchar (r);
+	}
+	else
+	}
+		r = (-(n % 10) + 48);
+		_putchar (0 + r);
+	}
+	return (4);
+}
