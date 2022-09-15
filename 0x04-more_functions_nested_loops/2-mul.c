f0f7be3652092cf61f70736bb4a4a30402a8918f
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-15 14:20:45 -0700

    2nd tax

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check the code.
+ * @a: - being tested.
+ * @b: - being tested.
+ * @c: - being tested.
+ * Return: multiplication of a and b
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
