cc6aa9a747836320446d1a5af47ba4068658dd0b
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-16 08:43:38 -0700

    My 4th task on more function and nested

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,17 @@
 /**
  * print_numbers - 1......9
  * @i: is the letter being tested
- * Return: 0
  *
  */
 
 void print_numbers(void)
 
 {
+	int i;
+
 	for (i = 48; i < 58; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar('\n');
-	return 0;
+	_putchar('\n');
 }
