ca8e1a4253e71db6b4a237cd98ba6ac974ea01d4
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-19 01:43:07 -0700

    my task 0 on Pointers,Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - change the vlause from 402 t0 98
+ * @n: the intergal
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	n = 402;
+
+	int *p = &n;
+
+	*p = 98;
+}
