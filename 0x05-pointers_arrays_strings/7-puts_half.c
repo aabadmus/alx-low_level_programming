79e1ec55ddb531ff9258bbbdaee60ca51938a193
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-20 02:50:57 -0700

    My 7th task on Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,30 @@
 void puts_half(char *str)
 {
 	int length = 0;
+	int n;
+	int r;
 
 	for (length = 0; str[length] != '\0'; length++)
 	{
-		if (length >= 5)
-		_putchar(str[length]);
+		length = length;
+	}
+	n = length / 2;
+	r = length - ((length - 1) / 2);
+
+	for (length = n; str[length] != '\0'; length++)
+	{
+		if (length % 2 == 0)
+		{
+			n = n;
+			_putchar(str[length]);
+		}
+
+		else
+		{
+			n = r;
+			_putchar(str[length]);
+		}
 	}
 	_putchar('\n');
+
 }
