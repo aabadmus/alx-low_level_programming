cfcd842ccbd8be28f4ae927df30ca282a464449c
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-09-20 03:39:58 -0700

    My 11th task

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,46 +1,42 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
+ * main - random password generator for 101-crackme
  *
- * Return: the int converted from the string
+ * Return: always 0
  */
-int _atoi(char *s)
+int main(void)
 {
-	int i, d, n, len, f, digit;
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
+		i = k = s = 0;
+		while ((2772 - 122) > s)
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
 		}
-		i++;
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
