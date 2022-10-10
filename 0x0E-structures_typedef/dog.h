08ab5f8d2cdd7e1b8bf22a7b642d8d35fb01ea0e
Ahmed Adekunle Badmus <aabadmus@yahoo.com>
2022-10-10 14:45:52 -0700

    MY task 0 on structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
-struct dog 
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
-};  
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
